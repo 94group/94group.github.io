b4d233fd1946a7f0144fd152325eb76d7891bd3c
shika233 <1692708036@qq.com>
2018-06-21 22:50:27 +0800

    Vue项目初始化，css样式初始化

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,13 @@ export default {
 </script>
 
 <style>
+@import url("//unpkg.com/element-ui@2.4.1/lib/theme-chalk/index.css");
+@import url("./style/common.scss");
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+import elementUI from 'element-ui'
+
+Vue.use(elementUI)
 Vue.config.productionTip = false
-Vue.use(ElementUI)
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
